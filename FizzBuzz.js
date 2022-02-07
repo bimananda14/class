16284e4d3d2190180472b695c337a714513dfb1d
bimananda14 <clash20949@gmail.com>
2022-02-07 17:34:33 +0530

    result in one line

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,7 +1,9 @@
+let output = "";
 for (let i=1; i<=100; i++)
 {
-    let output = "";
-    if (i%3==0) output += "Fizz";
-    if (i%5==0) output += "Buzz";
-    console.log(output || i)
+    if (i%3==0 && i%5==0) output += "FizzBuzz ";
+    else if (i%3==0) output += "Fizz ";
+    else if (i%5==0) output += "Buzz ";
+    else output += i + " ";
 }
+console.log(output);
